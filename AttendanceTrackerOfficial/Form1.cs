8f8f7a95e107fc192cda542380bcf1852d4e18a8
ashlydavin436-boop <ashlydavin436@gmail.com>
2025-09-17 22:58:45 +0800

    updated

--- AttendanceTrackerOfficial/Form1.cs
+++ AttendanceTrackerOfficial/Form1.cs
@@ -7,8 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 
 namespace AttendanceTrackerOfficial
@@ -16,7 +15,7 @@ namespace AttendanceTrackerOfficial
  
     public partial class Signup : Form
     {
-        SqlConnection connect = new SqlConnection("Data Source=GEMINI;Initial Catalog=StudentAttendanceTracker_db;Integrated Security=True;TrustServerCertificate=True");
+        SqlConnection connect = new SqlConnection("Data Source=GEMINI;InitialCatalog=StudentAttendanceTracker_db;IntegratedSecurity=True;TrustServerCertificate=True");
 
         public Signup()
         { 
@@ -32,7 +31,61 @@ namespace AttendanceTrackerOfficial
         private void button1_Click(object sender, EventArgs e)
         {
 
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+                    String checkUsername = "SELECT * FROM admin WHERE username = '"
+                         + signup_username.Text.Trim() + "'"; //admin is out table name
+
+                    using (SqlCommand checkUser = new SqlCommand(checkUsername, connect))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(checkUser);
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+
+                        if (table.Rows.Count >= 1)
+                        {
+                            MessageBox.Show(signup_username.Text + " is already exist", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            string insertData = "INSERT INTO admin (email, username, password, date_created) " +
+                                "VALUES(@email, @username, @pass, @date)";
+
+                            DateTime date = DateTime.Today;
+
+                            using (SqlCommand cmd = new SqlCommand(insertData, connect))
+                            {
+                                cmd.Parameters.AddWithValue("@email", signup_email.Text.Trim());
+                                cmd.Parameters.AddWithValue("@username", signup_username.Text.Trim());
+                                cmd.Parameters.AddWithValue("@pass", signup_password.Text.Trim());
+                                cmd.Parameters.AddWithValue("@date", date);
+
+                                cmd.ExecuteNonQuery(); // execute the command!
+
+                                MessageBox.Show("Registered successfully", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                //To Switch the form
+                                Signup loginForm = new Signup();
+                                loginForm.Show();
+                                this.Close();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error connecting Database: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
         }
+        
 
         private void label4_Click(object sender, EventArgs e)
         {
